ebc95a987e2324b173119ab4836573a720da308e
Kevin Traver <kevintraver@gmail.com>
2011-09-19 12:28:45 -0700

    Server now shows receiveData

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -7,24 +7,28 @@ class UDPServer {
   
       DatagramSocket serverSocket = new DatagramSocket(9876); 
   
-      byte[] receiveData = new byte[1024]; 
+      
       byte[] sendData  = new byte[1024]; 
   
       while(true) 
         { 
-  
+    	  byte[] receiveData = new byte[1024]; 
+    	  
+    	  String sentence = "";
+    	  String capitalizedSentence =  "";
           DatagramPacket receivePacket = 
              new DatagramPacket(receiveData, receiveData.length); 
            serverSocket.receive(receivePacket); 
-          String sentence = new String(receivePacket.getData()); 
+          sentence = new String(receivePacket.getData()); 
   
           InetAddress IPAddress = receivePacket.getAddress(); 
   
           int port = receivePacket.getPort(); 
   
-                      String capitalizedSentence = sentence.toUpperCase(); 
+                      capitalizedSentence = sentence.toUpperCase(); 
 
           sendData = capitalizedSentence.getBytes(); 
+          System.out.println(capitalizedSentence);
   
           DatagramPacket sendPacket = 
              new DatagramPacket(sendData, sendData.length, IPAddress, 
